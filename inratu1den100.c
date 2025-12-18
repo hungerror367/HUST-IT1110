da18e438cd31e24febe66947dbbc6722023f9f7b
Hung Nguyen | Developer <nkhungg367@gmail.com>
2025-12-18 13:58:09 +0700

    Add files via upload

---
+++ inratu1den100.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i=1,n;
+    scanf("%d",&n);
+/*    while(i<=n){
+        printf("\n%d",i);
+        i++;
+    }
+*/
+    for(i=1;i<=n;i++)
+        printf("\n%d",i);
+    return 0;
+}
